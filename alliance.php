7c175a7ca69c2052916aa5aacaca840e6e89f078
Mousshaker <mousshk@gmail.com>
2016-12-06 17:05:26 +0100

    optimisation alliance.php

--- alliance.php
+++ alliance.php
@@ -37,31 +37,34 @@ if($timeStamp !=""){
 
 }
 
- 
+$serverNumber = 144; # ID de votre serveur (Uni)
+$idPlayer = 100178; # votre ID de joueur
 						
 
-### Info sur un joueur ciblé ###
 ?>
 <div class="menuHead">
-<?php
-# Génération du Menu
-echo '<a href="https://s144-fr.ogame.gameforge.com/api/players.xml" class="menuPadding" target="_blank">XML players Global</a>';
-echo '<a href="https://s144-fr.ogame.gameforge.com/api/playerData.xml?id=100178" class="menuPadding" target="_blank">XML player</a>';
-?>
+	<?php
+	# Génération du Menu
+	echo '<a href="https://s'.$serverNumber.'-fr.ogame.gameforge.com/api/players.xml" class="menuPadding" target="_blank">XML players Global</a>';
+	echo '<a href="https://s'.$serverNumber.'-fr.ogame.gameforge.com/api/playerData.xml?id='.$idPlayer.'" class="menuPadding" target="_blank">XML player</a>';
+	?>
 </div>
+
+<!-- Info sur un joueur ciblé -->
 <div style="overflow-x:auto;" id="player" name="player" class="panelDark">
 	<div id="Title" class="headPanelTitle">
 		<h2>Info joueur</h2>
 	</div>
 	<div class="center intro">
 	En entrant le nom d'un joueur, vous pourrez obtenir toutes ses colonnies.</br>
-	Notez que les informations sont automatiquement mises à jour 1 fois par SEMAINE.
+	Notez que les informations sont mises à jour 1 fois par SEMAINE seulement (par ogame).
 	</div>
 	<div id="infoPlayer" class="center">
 		<?php
 		if(isset($_POST['namePlayer'])){
 			$player = array();
-			$xmlAll = simplexml_load_file('https://s144-fr.ogame.gameforge.com/api/players.xml');
+			$xmlAll = simplexml_load_file('https://s'.$serverNumber.'-fr.ogame.gameforge.com/api/players.xml');
+			#remplacer '144' par le numéro du serveur de votre Uni.
 			foreach ($xmlAll->player as $value) {
 				if(strtolower($value['name'])==strtolower($_POST['namePlayer'])){
 					$player[] = array($value['id'],$value['name']);
@@ -69,9 +72,9 @@ echo '<a href="https://s144-fr.ogame.gameforge.com/api/playerData.xml?id=100178"
 				
 			}
 			if($player[0][0]!=""){
-				$xmlPlayer = simplexml_load_file('https://s144-fr.ogame.gameforge.com/api/playerData.xml?id='.$player[0][0]);
+				$xmlPlayer = simplexml_load_file('https://s'.$serverNumber.'-fr.ogame.gameforge.com/api/playerData.xml?id='.$player[0][0]);
 				echo '<table>';
-				echo '<tr><td>Joueur</td><th>'.$player[0][1].' </th></tr> ';
+				echo '<tr><td>Joueur</td><th>'.$player[0][1].' (ID '.$player[0][0].')</th></tr> ';
 				echo '<tr><td>Planètes</td><td>';
 					foreach ($xmlPlayer->planets->planet as $key => $value) {
 							echo '<font class="cyanResult">'.$value['name'].' ['.$value['coords'].'] </font>';
@@ -103,21 +106,20 @@ echo '<a href="https://s144-fr.ogame.gameforge.com/api/playerData.xml?id=100178"
 	<div id="Title" class="headPanelTitle">
 		<h2>Alliance</h2>
 		<?php
-		$xmlPlayer = simplexml_load_file('https://s144-fr.ogame.gameforge.com/api/playerData.xml?id=100178');
+		$xmlPlayer = simplexml_load_file('https://s'.$serverNumber.'-fr.ogame.gameforge.com/api/playerData.xml?id='.$idPlayer);
 		foreach ($xmlPlayer->alliance as $value) {
 			echo '<font class="infoStandard">'.$value->name.' ['.$value->tag.'] ';
 		}
 		?>
 	</div>
 <?php
+
 ## INFOS ALLIANCE ##
-//$xmlstr='https://s144-fr.ogame.gameforge.com/api/playerData.xml?id=100178';
-
-
 $idPlayer = array();
-$xmlAll = simplexml_load_file('https://s144-fr.ogame.gameforge.com/api/players.xml');
+$idAlliance = 27; # remplaçer par l'ID de votre alliance
+$xmlAll = simplexml_load_file('https://s'.$serverNumber.'-fr.ogame.gameforge.com/api/players.xml');
 foreach ($xmlAll->player as $value) {
-	if($value['alliance']=="27"){
+	if($value['alliance']==$idAlliance){
 		//echo $value['name'].' ('.$value['id'].')<br>';
 		$idPlayer[] = array($value['id'],$value['name']);
 	}
@@ -132,7 +134,7 @@ echo '</tr>';
 	foreach ($idPlayer as $keyPlayer => $valuePlayer) {
 		echo '<tr>';
 		echo '<th>'.$idPlayer[$keyPlayer][1].' </th> ';
-		$xmlPlayer = simplexml_load_file('https://s144-fr.ogame.gameforge.com/api/playerData.xml?id='.$idPlayer[$keyPlayer][0]);
+		$xmlPlayer = simplexml_load_file('https://s'.$serverNumber.'-fr.ogame.gameforge.com/api/playerData.xml?id='.$idPlayer[$keyPlayer][0]);
 		for($i=1;$i<8;$i++){
 			echo '<td><table>';
 			foreach ($xmlPlayer->planets->planet as $key => $value) {
